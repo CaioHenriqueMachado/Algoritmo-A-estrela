17327cd185d3d2a68c88315ce474075b14ee03b2
Caio <caio.student@outlook.com>
2020-09-28 23:22:50 -0300

    ajustando identação

--- estrela.py
+++ estrela.py
@@ -1,121 +1,118 @@
 class Aresta:
-    def __init__(self, alvo,custo):
-        self.custo=custo
-        self.alvo=alvo
-  
-    def getCusto(self):
-        return self.custo
-  
-    def getAlvo(self):
-        return self.alvo
+  def __init__(self, alvo,custo):
+    self.custo=custo
+    self.alvo=alvo
+
+  def getCusto(self):
+    return self.custo
+
+  def getAlvo(self):
+    return self.alvo
 
 class No:
-    def __init__(self, nome, funcaoH):
-        self.nome=nome
-        self.funcaoH=funcaoH
-        self.funcaoF=0
-        self.adjacente=None
+  def __init__(self, nome, funcaoH):
+    self.nome=nome
+    self.funcaoH=funcaoH
+    self.funcaoF=0
+    self.adjacente=None
 
-    def getNome(self):
-        return self.nome
-    
-    def getFuncaoH(self):
-        return self.funcaoH
+  def getNome(self):
+    return self.nome
+  
+  def getFuncaoH(self):
+    return self.funcaoH
 
-    def setFuncaoF(self, funcaoF):
-        self.funcaoF = funcaoF
-    
-    def getFuncaoF(self):
-        return self.funcaoF
-    
-    def setFuncaoG(self, funcaoG):
-        self.funcaoG=funcaoG
+  def setFuncaoF(self, funcaoF):
+    self.funcaoF = funcaoF
+  
+  def getFuncaoF(self):
+    return self.funcaoF
+  
+  def setFuncaoG(self, funcaoG):
+    self.funcaoG=funcaoG
 
-    def getFuncaoG(self):
-        return self.funcaoG
-    
-    def setAdjacente(self, adjacente):
-        self.adjacente=adjacente
-    
-    def getAdjacente(self):
-        return self.adjacente
-    
-    def setAdjacentes(self, adjacentes):
-        self.adjacentes=adjacentes
-    
-    def getAdjacentes(self):
-        return self.adjacentes
+  def getFuncaoG(self):
+    return self.funcaoG
+  
+  def setAdjacente(self, adjacente):
+    self.adjacente=adjacente
+  
+  def getAdjacente(self):
+    return self.adjacente
+  
+  def setAdjacentes(self, adjacentes):
+    self.adjacentes=adjacentes
+  
+  def getAdjacentes(self):
+    return self.adjacentes
 
 
 def encontrarMenor(lista):
-    menor = 0
-    for indice in range(0,len(lista)):
-        if(lista[menor].getFuncaoF()>lista[indice].getFuncaoF()):
-            menor = indice
-    return lista.pop(menor)
+  menor = 0
+  for indice in range(0,len(lista)):
+    if(lista[menor].getFuncaoF()>lista[indice].getFuncaoF()):
+      menor = indice
+  return lista.pop(menor)
 
 def percorrerCaminho(alvo):
-    caminho = "]"
-    while(not(alvo is None)):
-       caminho=", "+alvo.getNome()+caminho
-       alvo=alvo.getAdjacente()
-    caminho = "["+caminho
-    return caminho
+  caminho = "]"
+  while(not(alvo is None)):
+    caminho=", "+alvo.getNome()+caminho
+    alvo=alvo.getAdjacente()
+  caminho = "["+caminho
+  return caminho
 
 
 def aEstrela(origem,fim): 
-    # 1. Criar fila explorados e fila de prioridades = {Ø}
-    fila=[]
-    explorados=[]
+  # 1. Criar fila explorados e fila de prioridades = {Ø}
+  fila=[]
+  explorados=[]
 
-    # 2. Se origem == destino -> finaliza
-    if(origem.getNome() != fim.getNome()):
-      # 3. Função g do nó origem = 0
-      origem.setFuncaoG(0)
-      # 4. filaPrioridades adiciona origem
-      fila.append(origem)
-      # 5. Enquanto filaPrioridades não vazia e destino não encontrado
-      encontrado=False
+  # 2. Se origem == destino -> finaliza
+  if(origem.getNome() != fim.getNome()):
+    # 3. Função g do nó origem = 0
+    origem.setFuncaoG(0)
+    # 4. filaPrioridades adiciona origem
+    fila.append(origem)
+    # 5. Enquanto filaPrioridades não vazia e destino não encontrado
+    encontrado=False
 
-      while(len(fila)>0 and not(encontrado)):
-        # 5_1. Nó atual = Nó com menor função f
-        atual = encontrarMenor(fila)
-        # 5_2. Fila explorados adiciona nó atual
-        explorados.append(atual)
-        # 5_3. Se nó atual == destino -> parar
-        if(atual.getNome()==fim.getNome()):
-          encontrado=True
+    while(len(fila)>0 and not(encontrado)):
+      # 5_1. Nó atual = Nó com menor função f
+      atual = encontrarMenor(fila)
+      # 5_2. Fila explorados adiciona nó atual
+      explorados.append(atual)
+      # 5_3. Se nó atual == destino -> parar
+      if(atual.getNome()==fim.getNome()):
+        encontrado=True
 
-        # 5_4. Se não...
-        else:
-          # 5_4_1. Para cada aresta adjacente do no atual
-          for aresta in atual.getAdjacentes():
-            # 5_4_2. Nó filho = aresta.alvo
-            filho = aresta.getAlvo()
-            # 5_4_3. funcaoGTemp= noAtual.FuncaoG() + aresta.custo
-            funcaoGTemp = atual.getFuncaoG() + aresta.getCusto()
-            # 5_4_4. funcaoFTemp= funcaoGTemp+ noFilho.FuncaoH()
-            funcaoFTemp= funcaoGTemp+filho.getFuncaoH()
-            # 5_4_5. Se caso o nó filho já tenha sido explorado e seu valor de função f é maior que a função f do pai, então ele é desconsiderado
-            if((filho in explorados) and (funcaoFTemp>=filho.getFuncaoF())):
-              continue
+      # 5_4. Se não...
+      else:
+        # 5_4_1. Para cada aresta adjacente do no atual
+        for aresta in atual.getAdjacentes():
+          # 5_4_2. Nó filho = aresta.alvo
+          filho = aresta.getAlvo()
+          # 5_4_3. funcaoGTemp= noAtual.FuncaoG() + aresta.custo
+          funcaoGTemp = atual.getFuncaoG() + aresta.getCusto()
+          # 5_4_4. funcaoFTemp= funcaoGTemp+ noFilho.FuncaoH()
+          funcaoFTemp= funcaoGTemp+filho.getFuncaoH()
+          # 5_4_5. Se caso o nó filho já tenha sido explorado e seu valor de função f é maior que a função f do pai, então ele é desconsiderado
+          if((filho in explorados) and (funcaoFTemp>=filho.getFuncaoF())):
+            continue
 
-            # 5_4_6. senão se o nó filho não está na filaPrioridadesou sua função f é maior que a funcaoFTemp
-            elif((filho not in fila)or (funcaoFTemp < filho.getFuncaoF())):
-              # 5_4_6_1. filho.Adjacente= atual
-              filho.setAdjacente(atual)
-              # 5_4_6_2.filho.funcaoG= funcaoGTemp
-              filho.setFuncaoG(funcaoGTemp)
-              # 5_4_6_3. filho.funcaoF= funcaoFTemp
-              filho.setFuncaoF(funcaoFTemp)
-              # 5_4_6_4. Se filaPrioridadescontem filho
-              # 5_4_6_5. fila.removefilho
-              if(filho not in fila):
-                # 5_4_6_5. Fila adiciona filho
-                fila.append(filho)
-  
-  
-      
+          # 5_4_6. senão se o nó filho não está na filaPrioridadesou sua função f é maior que a funcaoFTemp
+          elif((filho not in fila)or (funcaoFTemp < filho.getFuncaoF())):
+            # 5_4_6_1. filho.Adjacente= atual
+            filho.setAdjacente(atual)
+            # 5_4_6_2.filho.funcaoG= funcaoGTemp
+            filho.setFuncaoG(funcaoGTemp)
+            # 5_4_6_3. filho.funcaoF= funcaoFTemp
+            filho.setFuncaoF(funcaoFTemp)
+            # 5_4_6_4. Se filaPrioridadescontem filho
+            # 5_4_6_5. fila.removefilho
+            if(filho not in fila):
+              # 5_4_6_5. Fila adiciona filho
+              fila.append(filho)
 
 # ---------------------------------------------------------------------------
 
